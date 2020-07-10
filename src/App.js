4b34a52d8bcca6b1708f6e8d74be898d3daa6fad
TARUN KISHORE <tarunksu30@gmail.com>
2020-07-10 13:14:19 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       num:1
+    }
+  }
+
+  randomNumber(){
+    let random = Math.floor(Math.random() * 10) + 1
+    this.setState({num:random})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Number:{this.state.num}</h1>
+        {this.state.num === 7 
+          ? <div><h2>You Win !</h2><button>Reset</button></div>
+          : <button onClick={()=>{this.randomNumber()}}>Click Me !!!</button>    
+        }
+            
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
